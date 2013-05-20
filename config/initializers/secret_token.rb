31ccff5f041f9b1d8da21a6d7c1e1edfa93fd203
Chris Meadows <meadoch1@gmail.com>
2013-05-20 19:58:06 -0400

    Initial checkin.  Basic Rails site with rspec and cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usermanage::Application.config.secret_token = '22857081118cb2a1b06fef2a1bad6e0663c0adeff4841e957cea28aecfcd529f909e1e1982faccb8e60ee0b3fc3456bb80dc9091ef482f6117c44c2e6ccb14f3'
